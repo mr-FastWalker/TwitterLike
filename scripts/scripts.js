f3492b6a6cc01cdea42961c8533fd65779c0d0dd
mr-FastWalker <mr.skorokhod@mail.ru>
2021-01-21 20:44:36 +1000

    first commit after first lecture

---
+++ scripts/scripts.js
@@ -0,0 +1,80 @@
+
+
+class Twitter {
+    constructor({ listElem }) {
+        this.tweets = new Posts ();
+        this.elements = {
+            listElem: document.querySelector(listElem)
+        }
+    }
+
+    renderPosts () {
+
+    }
+
+    showUserPost () {
+
+    }
+
+    showLikesPost () {
+
+    }
+
+    showAllPost () {
+
+    }
+    
+    openModal () {
+
+    }
+
+
+}
+
+class Posts {
+    constructor ( {posts = [] } = {} ) {
+        this.posts = posts;
+
+    }
+
+    addPost (tweet) {
+        this.posts.push(new Post (tweet));
+    }
+
+    deletePost (id) {
+
+    }
+
+    likePost (id) {
+
+    }
+}
+
+
+class Post {
+    constructor (param) {
+        this.id = param.id;
+        this.userName = param.userName;
+        this.nickName = param.nickName;
+        this.postDate = param.postDate;
+        this.text = param.text;
+        this.img = param.img;
+        this.likes = param.likes;
+        this.liked = false;
+    }
+
+    changeLike(){
+        this.liked != this.liked;
+        if (this.liked) {
+            this.likes++;
+        } else {
+            this.likes--;
+        }
+    };
+}
+
+const twitter = new Twitter ({
+    listElem: '.tweet-list'
+})
+
+console.log ('twitter: ', twitter);
